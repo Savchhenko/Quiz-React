1903ebdc863471bc71562491238f32377f8ce74d
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-25 01:12:40 +0300

    add react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
-    <div>
+    <Router>
       <h1>Hello</h1>
-    </div>
+    </Router>
   );
 }
 
