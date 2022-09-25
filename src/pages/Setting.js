7dad5b601c88f4e5be6522c8c544e8b092e51a33
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-25 19:03:08 +0300

    add CircularProgress during page loading

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,14 +1,23 @@
-import { Button, Typography } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import SelectField from "../components/SelectField";
 import TextFieldComp from "../components/TextFieldComp";
 import useAxios from "../hooks/useAxios";
 
 const Setting = () => {
-    const { response, error, loading } = useAxios({ url: "api_category"});
+    const { response, error, loading } = useAxios({ url: "api_category.php"});
+    
     const handleSubmit = (e) => {
         e.preventDefault();
     };
+
+    if (loading) {
+        return (
+            <Box mt={20}>
+                <CircularProgress />
+            </Box>
+        )
+    }
 
     return (
         <>
